415889f9267790d8649aa4addd97d94fbe247e1b
Alden Hinden-Stevenson <aldenhinden@gmail.com>
2023-05-02 15:20:09 -0700

    mocha testing for webscraping

---
+++ test/back-end/webscrape/webscraping.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const { scrapePdf } = require('../../../src/back-end/webscrape/index.js');
+
+function describe(scrapePdf, param2) {
+    
+}
+
+describe('scrapePdf', () => {
+    it('should return the text content of a PDF file', () => {
+        // Your test code goes here
+    });
+});
