f282c88b8b957e70267b14cbe12c175468b3a6bb
johnli01 <john.li9631@gmail.com>
2023-05-04 03:36:47 -0700

    reformated translation page buttons, client retrieved uploaded pdf file from client

--- src/ui/src/app/modules/translate/translate.component.ts
+++ src/ui/src/app/modules/translate/translate.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class TranslateComponent {
 
+  ngOnInit(): void{
+  }
+
+  EMPTY_FORM = new FormData();
+  curr_file = this.EMPTY_FORM;
+
+  onChange(event: any) {
+    if (event.target.files.length > 0 && event.target.files[0].type == "application/pdf") {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('file', file);
+        
+      this.curr_file = formData;
+    } else {
+      this.curr_file = this.EMPTY_FORM;
+    }
+  }
+
 }
