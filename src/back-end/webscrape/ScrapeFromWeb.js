1a8cca1ed9dc565b7837bcc83868991108f70443
johnli01 <john.li9631@gmail.com>
2023-05-07 03:40:59 -0700

    set express port as 3000, improved error handling on null files

--- src/back-end/webscrape/ScrapeFromWeb.js
+++ src/back-end/webscrape/ScrapeFromWeb.js
@@ -11,24 +11,29 @@ app.use(cors());
 
 // define the routing
 app.post('/upload/', fileUpload( {createParentPath: true}), (req, res) => {
-    console.log(req);
+    if (req.files == null) {
+        return res.json({ status: "error", text: "" });
+    }
+
+    // saves uploaded file into memory into samplePDFs
     fs.writeFileSync('samplePDFs/'+req.files.file.name, req.files.file.data);
 
     // read the file into memory from routing
     const file_buffer = fs.readFileSync('samplePDFs/'+req.files.file.name);
-    // test locally by adding           ^ 'samplePDFs/'+  here before req.params... since my sample is in a folder
-    // then navigate to localhost:3000/pdf/chocolate_chip_cookies.pdf <- should print text from that pdf
 
     // parse the PDF and extract the text content
-
     pdfParse(file_buffer).then(function (pdf_data) {
-        // send the text back to the client
-        console.log(pdf_data);
-        res.send(pdf_data.text);
+        // sends success message and txt to client if successful, else error
+        if (pdf_data != null) {
+            return res.json({ status: "uploaded", text: pdf_data.text });
+
+        } else {
+            return res.json({ status: "error", text: "" });
+        }
     });
 });
 
 // start the server
-app.listen(3500, function() {
-    console.log('Server is listening on port 3500');
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000');
 });
