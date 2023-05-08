d44147b8019d2528478ae9f3a4ec9899a3a43799
Yusuf <ydodi19@gmail.com>
2023-05-07 17:03:05 -0700

    Some brief comments for chatGPT integration.

--- src/back-end/integration/index.js
+++ src/back-end/integration/index.js
@@ -2,9 +2,11 @@ const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
 const configuration = new Configuration({
+  // Read and save API key stored in .env
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+// Prompt saved in variable input_prompt
 input_prompt = "What animal is stronger, Tiger or Lion?"
 async function runCompletion () {
 const completion = await openai.createCompletion({
@@ -12,6 +14,7 @@ const completion = await openai.createCompletion({
   prompt: input_prompt,
   max_tokens: 2049,
 });
+// Print reply to console FOR NOW. Will send to front-end.
 console.log(completion.data.choices[0].text);
 }
 runCompletion();
