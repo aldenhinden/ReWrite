9be932d9c37674667928b4be89fb1f4f96e1dacc
johnli01 <john.li9631@gmail.com>
2023-05-31 18:31:36 -0700

    rerouted post to work with prod

--- src/ui/src/app/modules/translate/translate.component.ts
+++ src/ui/src/app/modules/translate/translate.component.ts
@@ -82,7 +82,7 @@ export class TranslateComponent {
     this.curr_file.set('type', this.translation_type);
     
     // Sends information to backend server to process
-    this.http.post('http://localhost:3000/upload', this.curr_file).subscribe(response => {
+    this.http.post('45.77.208.169:3000/upload', this.curr_file).subscribe(response => {
       console.log(response);
       let res = JSON.parse(JSON.stringify(response));
       let simplified = res.text;
